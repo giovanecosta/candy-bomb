95cd9c112a1fb5e3dac3d8b71ee7ae1d25356ffd
giovane <giovane@vialink.com.br>
2014-10-08 10:12:23 -0300

    Setting Module Factory layout

--- yii/modules/factory/controllers/DefaultController.php
+++ yii/modules/factory/controllers/DefaultController.php
@@ -6,8 +6,12 @@ use yii\web\Controller;
 
 class DefaultController extends Controller
 {
+
     public function actionIndex()
     {
+
+        $this->layout = 'main';
+
         return $this->render('index');
     }
 }
