6a105c381be53184976e0a2daf8ec71502e8c5ef
giovane <giovane@vialink.com.br>
2014-10-10 00:30:08 -0300

    Renaming smart field / adding unique index

--- yii/migrations/m141009_132719_create_polyiigon_smart_fields.php
+++ yii/migrations/m141009_132719_create_polyiigon_smart_fields.php
@@ -3,11 +3,11 @@
 use yii\db\Schema;
 use yii\db\Migration;
 
-class m141009_132719_create_smart_fields extends Migration
+class m141009_132719_create_polyiigon_smart_fields extends Migration
 {
     public function up()
     {
-    	$this->createTable('smart_fields', [
+    	$this->createTable('polyiigon_smart_fields', [
             'id' => 'pk',
             // Just the name of the field
             'title' => Schema::TYPE_STRING . ' NOT NULL',
@@ -17,10 +17,13 @@ class m141009_132719_create_smart_fields extends Migration
             // that will do the 'smart things' for this field
             'flag' => Schema::TYPE_STRING . ' NOT NULL'
         ]);
+
+        $this->createIndex('unique_flag', 'polyiigon_smart_fields', 'flag', true);
     }
 
     public function down()
     {
-        $this->dropTable('smart_fields');
+    	$this->dropIndex('unique_flag', 'polyiigon_smart_fields');
+        $this->dropTable('polyiigon_smart_fields');
     }
 }
