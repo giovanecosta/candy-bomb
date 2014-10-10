b8882195a91cc26cd861e0c42fe43db709a46f81
giovane <giovane@vialink.com.br>
2014-10-10 01:30:17 -0300

    Model for smart field table

---
+++ yii/models/PolyiigonSmartFields.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "polyiigon_smart_fields".
+ *
+ * @property integer $id
+ * @property string $title
+ * @property string $schema_field
+ * @property string $flag
+ */
+class PolyiigonSmartFields extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'polyiigon_smart_fields';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['title', 'schema_field', 'flag'], 'required'],
+            [['title', 'schema_field', 'flag'], 'string', 'max' => 255],
+            [['flag'], 'unique']
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'schema_field' => 'Schema Field',
+            'flag' => 'Flag',
+        ];
+    }
+}
