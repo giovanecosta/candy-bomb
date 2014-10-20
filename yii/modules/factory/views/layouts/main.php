b086d8ddd04039bcea4497f4e8a734b3155b47ca
giovane <giovane@vialink.com.br>
2014-10-20 01:41:52 -0200

    Highlighting form valid/invalid

--- yii/modules/factory/views/layouts/main.php
+++ yii/modules/factory/views/layouts/main.php
@@ -28,17 +28,17 @@ FactoryAsset::register($this);
 
         <div class="container">
 
-            <form class="form-horizontal" role="form">
+            <form class="form-horizontal" ng-class="{submited: moduleTemplateForm.submited}" name="moduleTemplateForm" role="form" ng-submit="save()" novalidate>
                 <div class="form-group" ng-repeat="field in fields">
                     <h3 class="col-sm-2 control-label">Campo {{$index + 1}}</h3>
                     <div class="row col-sm-10">
                         <div class="col-xs-3">
                             <label for="mtFieldName{{$index}}">Nome</label>
-                            <input type="text" class="form-control" ng-model="field.name" id="mtFieldName{{$index}}" placeholder="Nome">
+                            <input type="text" class="form-control" ng-model="field.name" id="mtFieldName{{$index}}" placeholder="Nome" required>
                         </div>
                         <div class="col-xs-2">
                             <label for="mtFieldSF{{$index}}">Tipo</label>
-                            <select id="mtFieldSF{{$index}}" class="form-control" ng-model="field.smartField" ng-options="sf.title for sf in smartFields"></select>
+                            <select id="mtFieldSF{{$index}}" class="form-control" ng-model="field.smartField" ng-options="sf.title for sf in smartFields | orderBy: 'title'" required></select>
                         </div>
                     </div>
                 </div>
@@ -46,14 +46,11 @@ FactoryAsset::register($this);
                 <div class="form-group">
                     <label class="col-sm-2 control-label"> &nbsp; </label>
                     <div class="row col-sm-10">
-                        <div class="col-xs-6">
-                            <button ng-click="doIt()" class="btn btn-default">Do it!</button>
-                        </div>
                         <div class="col-xs-2">
                             <button ng-click="fields.push({})" class="btn btn-default">Adicionar</button>
                         </div>
                         <div class="col-xs-2">
-                            <button ng-click="save()" class="btn btn-default">Salvar</button>
+                            <button type="submit" class="btn btn-default">Salvar</button>
                         </div>
                     </div>
                 </div>
--- yii/web/modules/factory/js/controllers/module_template.js
+++ yii/web/modules/factory/js/controllers/module_template.js
@@ -6,18 +6,24 @@ factoryApp.controller('ModuleTemplateCtrl', function ($scope, $http) {
 
 	$http.get('polyiigon-smart-field').success(function(data) {
 
-		$scope.smartFields = data.sort(function(a, b){ return (a.title == b.title) ? 0 : (a.title > b.title ? 1 : -1); });
+		$scope.smartFields = data;
 	});
 
 	$scope.save = function(){
-		data = {
-			name: 'Teste',
-			fields: $scope.fields
-		}
 
-		$http.post('polyiigon-module-template', data).success(function(data) {
-			console.log(data);
-		});
+		if ($scope.moduleTemplateForm.$valid){
+			data = {
+				name: 'Teste',
+				fields: $scope.fields
+			}
+
+			$http.post('polyiigon-module-template', data).success(function(data) {
+				console.log(data);
+			});
+		} else {
+
+			$scope.moduleTemplateForm.submited = true;
+		}
 	}
 
 	$scope.doIt = function(){
