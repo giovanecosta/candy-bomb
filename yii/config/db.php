8232213744ac617ae9a04d5c88637743ef62cecc
giovane <giovane@vialink.com.br>
2014-10-20 04:35:32 -0200

    Local db config

--- yii/config/db.php
+++ yii/config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=polyiigon',
     'username' => 'root',
-    'password' => '',
+    'password' => 'secret',
     'charset' => 'utf8',
 ];
