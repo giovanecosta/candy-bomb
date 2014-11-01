6f3a124e3561d0573aeb3f6c73fe23a5005d4b21
giovane <giovane@vialink.com.br>
2014-11-01 21:41:25 -0200

    Rotas de módulo

--- yii/web/modules/factory/js/route.js
+++ yii/web/modules/factory/js/route.js
@@ -1,12 +1,20 @@
 factoryApp.config(['$routeProvider', function($routeProvider) {
   $routeProvider.
     when('/module_template/:action/:id?', {
-      templateUrl: '/modules/factory/views/module_template_form.html',
+      templateUrl: '/modules/factory/views/module_template/form.html',
       controller: 'ModuleTemplateCtrl'
     }).
     when('/module_template', {
-      templateUrl: '/modules/factory/views/module_template_list.html',
+      templateUrl: '/modules/factory/views/module_template/list.html',
       controller: 'ModuleTemplateCtrl'
+    }).
+    when('/module/:action/:id?', {
+      templateUrl: '/modules/factory/views/module/form.html',
+      controller: 'ModuleCtrl'
+    }).
+    when('/module', {
+      templateUrl: '/modules/factory/views/module/list.html',
+      controller: 'ModuleCtrl'
     }).
     otherwise({
       redirectTo: '/'
