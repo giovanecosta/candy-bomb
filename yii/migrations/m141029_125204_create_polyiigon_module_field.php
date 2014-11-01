ca14169024803c2d954b12ba44d289bef865bb25
giovane <giovane@vialink.com.br>
2014-11-01 17:31:47 -0200

    Migrations de modulo

---
+++ yii/migrations/m141029_125204_create_polyiigon_module_field.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m141029_125204_create_polyiigon_module_field extends Migration
+{
+    public function up()
+    {
+        $this->createTable('polyiigon_module_field', [
+            'id' => 'pk',
+            // Just the name of the field
+            'title' => Schema::TYPE_STRING . ' NOT NULL',
+            // Template field
+            'polyiigon_module_template_field_id' => Schema::TYPE_INTEGER . ' NOT NULL',
+            // Module table
+            'polyiigon_module_id' => Schema::TYPE_INTEGER . ' NOT NULL',
+        ]);
+        // Template field
+        $this->addForeignKey('polyiigon_mf_polyiigon_mtf_fk', 'polyiigon_module_field', 'polyiigon_module_template_field_id', 'polyiigon_module_template_field', 'id', 'CASCADE', 'CASCADE');
+        // module template
+        $this->addForeignKey('polyiigon_mf_polyiigon_mod_fk', 'polyiigon_module_field', 'polyiigon_module_id', 'polyiigon_module', 'id', 'CASCADE', 'CASCADE');
+    }
+
+    public function down()
+    {
+        $this->dropForeignKey('polyiigon_mf_polyiigon_mtf_fk', 'polyiigon_module_field');
+
+        $this->dropForeignKey('polyiigon_mf_polyiigon_mod_fk', 'polyiigon_module_field');
+
+        $this->dropTable('polyiigon_module_field');
+    }
+}
