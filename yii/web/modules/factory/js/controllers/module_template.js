69f5dd16a737406111653a73c6374a7e691d99cc
giovane <giovane@vialink.com.br>
2014-11-01 17:31:29 -0200

    Implementando list e delete no module tmeplate

--- yii/web/modules/factory/js/controllers/module_template.js
+++ yii/web/modules/factory/js/controllers/module_template.js
@@ -1,25 +1,61 @@
-var factoryApp = angular.module('FactoryApp', []);
 
-factoryApp.controller('ModuleTemplateCtrl', function ($scope, $http) {
+factoryApp.controller('ModuleTemplateCtrl', function ($scope, $http, $routeParams, $location, Slug) {
 
 	$scope.fields = [{}];
 
-	$http.get('polyiigon-smart-field').success(function(data) {
+	$scope.updateList = function() {
+		$http.get('polyiigon-module-template').success(function(data) {
 
-		$scope.smartFields = data;
-	});
+			$scope.moduleTemplates = data;
+		});
+	}
+
+	if (['create', 'edit'].indexOf($routeParams.action) >= 0){
+
+		$http.get('polyiigon-smart-field').success(function(data) {
+
+			$scope.smartFields = data;
+		});
+	} else {
+		$scope.updateList();
+	}
+
+	$scope.delete = function(moduleTemplate){
+		$http.delete('polyiigon-module-template/delete?id='+moduleTemplate.id).success(function(data) {
+			$scope.updateList();
+		});
+	}
 
 	$scope.save = function(){
 
 		if ($scope.moduleTemplateForm.$valid){
 			data = {
-				title: 'Teste',
-				slug:'dfsdfsf',
-				//fields: $scope.fields
+				title: $scope.name,
+				slug: Slug.slugify($scope.name),
+			};
+
+			if ($routeParams.id){
+				data.id = $routeParams.action;
 			}
 
 			$http.post('polyiigon-module-template/create', data).success(function(data) {
-				console.log(data);
+				mtId = data.id;
+
+				for (f of $scope.fields){
+					fieldData = {
+						title: f.title,
+						polyiigon_smart_field_id: f.smartField.id,
+						polyiigon_module_template_id: mtId
+					};
+					if (f.id){
+						fieldData.id = f.id
+					}
+					$http.post('polyiigon-module-template-field/create', fieldData).success(function(data) {
+						console.log(data);
+						$location.path('/');
+					});
+				}
+
 			});
 		} else {
 
@@ -27,7 +63,5 @@ factoryApp.controller('ModuleTemplateCtrl', function ($scope, $http) {
 		}
 	}
 
-	$scope.doIt = function(){
-		console.log($scope.fields);
-	}
+	
 });
